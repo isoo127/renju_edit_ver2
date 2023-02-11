1c0b4a0db528ad55f47909544f81a11a5cb54b7d
Sang-Min Kang <rdrniel12345@gmail.com>
2023-02-11 09:05:38 +0900

    setting manifest file

--- app/src/main/java/com/renju_note/isoo/MainActivity.kt
+++ app/src/main/java/com/renju_note/isoo/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.renju_note.isoo
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var webView : WebView
 
     @SuppressLint("ClickableViewAccessibility", "SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
